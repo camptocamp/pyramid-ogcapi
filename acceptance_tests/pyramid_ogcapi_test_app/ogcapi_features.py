9a5de279933b8fe97c6f59534d58297be8986b47
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-09-07 09:46:26 +0200

    Rengenerate the types

--- acceptance_tests/pyramid_ogcapi_test_app/ogcapi_features.py
+++ acceptance_tests/pyramid_ogcapi_test_app/ogcapi_features.py
@@ -62,10 +62,16 @@ class CollectionsCollectionidGetResponse200(TypedDict, total=False):
     example: An address.
     """
 
-    links: Required[list["_Collectionscollectionidgetresponse200LinksItem"]]
+    links: Required[list["_ComponentsSchemasLink"]]
     """ Required property """
 
-    extent: "_Collectionscollectionidgetresponse200Extent"
+    extent: "_ComponentsSchemasExtent"
+    """
+    The extent of the features in the collection. In the Core only spatial and temporal
+    extents are specified. Extensions may add additional members to represent other
+    extents, for example, thermal or pressure ranges.
+    """
+
     itemType: str
     """
     indicator about the type of the items in the collection (the default value is 'feature').
@@ -170,8 +176,22 @@ class CollectionsCollectionidItemsGetQuery(TypedDict, total=False):
     """
 
     limit: "CollectionsCollectionidItemsGetQueryLimit"
-    bbox: list[Union[int, float]]
+    """
+    minimum: 1
+    maximum: 10000
+    default: 10
+    """
+
+    bbox: "CollectionsCollectionidItemsGetQueryBbox"
+    """ Aggregation type: oneOf """
+
     datetime: str
+
+
+CollectionsCollectionidItemsGetQueryBbox = Union[
+    "_CollectionscollectioniditemsgetquerybboxOneof0", "_CollectionscollectioniditemsgetquerybboxOneof1"
+]
+""" Aggregation type: oneOf """
 
 
 CollectionsCollectionidItemsGetQueryLimit = int
@@ -299,18 +319,6 @@ _COLLECTIONSCOLLECTIONIDGETRESPONSE200_CRS_DEFAULT = ["http://www.opengis.net/de
 """ Default value of the field path 'CollectionsCollectionidGetResponse200 crs' """
 
 
-_COLLECTIONSCOLLECTIONIDGETRESPONSE200_EXTENT_SPATIAL_CRS_DEFAULT = (
-    "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
-)
-""" Default value of the field path 'CollectionsCollectionidGetResponse200 extent spatial crs' """
-
-
-_COLLECTIONSCOLLECTIONIDGETRESPONSE200_EXTENT_TEMPORAL_TRS_DEFAULT = (
-    "http://www.opengis.net/def/uom/ISO-8601/0/Gregorian"
-)
-""" Default value of the field path 'CollectionsCollectionidGetResponse200 extent temporal trs' """
-
-
 _COLLECTIONSCOLLECTIONIDGETRESPONSE200_ITEMTYPE_DEFAULT = "feature"
 """ Default value of the field path 'CollectionsCollectionidGetResponse200 itemType' """
 
@@ -335,139 +343,22 @@ _COMPONENTS_SCHEMAS_EXTENT_TEMPORAL_TRS_DEFAULT = "http://www.opengis.net/def/uo
 """ Default value of the field path 'components schemas extent temporal trs' """
 
 
-class _Collectionscollectionidgetresponse200Extent(TypedDict, total=False):
-    """
-    The extent of the features in the collection. In the Core only spatial and temporal
-    extents are specified. Extensions may add additional members to represent other
-    extents, for example, thermal or pressure ranges.
-    """
-
-    spatial: "_Collectionscollectionidgetresponse200ExtentSpatial"
-    temporal: "_Collectionscollectionidgetresponse200ExtentTemporal"
-
-
-class _Collectionscollectionidgetresponse200ExtentSpatial(TypedDict, total=False):
-    """The spatial extent of the features in the collection."""
-
-    bbox: list["_Collectionscollectionidgetresponse200ExtentSpatialBboxItem"]
-    """
-    One or more bounding boxes that describe the spatial extent of the dataset.
-    In the Core only a single bounding box is supported. Extensions may support
-    additional areas. If multiple areas are provided, the union of the bounding
-    boxes describes the spatial extent.
-
-    minItems: 1
-    """
-
-    crs: "_Collectionscollectionidgetresponse200ExtentSpatialCrs"
-
-
-_Collectionscollectionidgetresponse200ExtentSpatialBboxItem = list[Union[int, float]]
-"""
-Each bounding box is provided as four or six numbers, depending on
-whether the coordinate reference system includes a vertical axis
-(height or depth):
-
-* Lower left corner, coordinate axis 1
-* Lower left corner, coordinate axis 2
-* Minimum value, coordinate axis 3 (optional)
-* Upper right corner, coordinate axis 1
-* Upper right corner, coordinate axis 2
-* Maximum value, coordinate axis 3 (optional)
-
-The coordinate reference system of the values is WGS 84 longitude/latitude
-(http://www.opengis.net/def/crs/OGC/1.3/CRS84) unless a different coordinate
-reference system is specified in `crs`.
-
-For WGS 84 longitude/latitude the values are in most cases the sequence of
-minimum longitude, minimum latitude, maximum longitude and maximum latitude.
-However, in cases where the box spans the antimeridian the first value
-(west-most box edge) is larger than the third value (east-most box edge).
-
-If the vertical axis is included, the third and the sixth number are
-the bottom and the top of the 3-dimensional bounding box.
-
-If a feature has multiple spatial geometry properties, it is the decision of the
-server whether only a single spatial geometry property is used to determine
-the extent or all relevant geometries.
-"""
-
-
-_Collectionscollectionidgetresponse200ExtentSpatialCrs = Union[
-    Literal["http://www.opengis.net/def/crs/OGC/1.3/CRS84"]
+_CollectionscollectioniditemsgetquerybboxOneof0 = Union[
+    str, Union[int, float], dict[str, Any], None, bool, None
 ]
 """
-Coordinate reference system of the coordinates in the spatial extent
-(property `bbox`). The default reference system is WGS 84 longitude/latitude.
-In the Core this is the only supported coordinate reference system.
-Extensions may support additional coordinate reference systems and add
-additional enum values.
-
-default: http://www.opengis.net/def/crs/OGC/1.3/CRS84
+minItems: 4
+maxItems: 4
 """
-_COLLECTIONSCOLLECTIONIDGETRESPONSE200EXTENTSPATIALCRS_HTTP_COLON__SOLIDUS__SOLIDUS_WWW_FULL_STOP_OPENGIS_FULL_STOP_NET_SOLIDUS_DEF_SOLIDUS_CRS_SOLIDUS_OGC_SOLIDUS_1_FULL_STOP_3_SOLIDUS_CRS84: Literal[
-    "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
-] = "http://www.opengis.net/def/crs/OGC/1.3/CRS84"
-"""The values for the 'Coordinate reference system of the coordinates in the spatial extent' enum"""
 
 
-class _Collectionscollectionidgetresponse200ExtentTemporal(TypedDict, total=False):
-    """The temporal extent of the features in the collection."""
-
-    interval: list["_Collectionscollectionidgetresponse200ExtentTemporalIntervalItem"]
-    """
-    One or more time intervals that describe the temporal extent of the dataset.
-    The value `null` is supported and indicates an unbounded interval end.
-    In the Core only a single time interval is supported. Extensions may support
-    multiple intervals. If multiple intervals are provided, the union of the
-    intervals describes the temporal extent.
-
-    minItems: 1
-    """
-
-    trs: "_Collectionscollectionidgetresponse200ExtentTemporalTrs"
-
-
-_Collectionscollectionidgetresponse200ExtentTemporalIntervalItem = list[
-    "_Collectionscollectionidgetresponse200ExtentTemporalIntervalItemItem"
+_CollectionscollectioniditemsgetquerybboxOneof1 = Union[
+    str, Union[int, float], dict[str, Any], None, bool, None
 ]
 """
-Begin and end times of the time interval. The timestamps are in the
-temporal coordinate reference system specified in `trs`. By default
-this is the Gregorian calendar.
-
-minItems: 2
-maxItems: 2
+minItems: 6
+maxItems: 6
 """
-
-
-_Collectionscollectionidgetresponse200ExtentTemporalIntervalItemItem = str
-"""
-format: date-time
-nullable: True
-"""
-
-
-_Collectionscollectionidgetresponse200ExtentTemporalTrs = Union[
-    Literal["http://www.opengis.net/def/uom/ISO-8601/0/Gregorian"]
-]
-"""
-Coordinate reference system of the coordinates in the temporal extent
-(property `interval`). The default reference system is the Gregorian calendar.
-In the Core this is the only supported temporal coordinate reference system.
-Extensions may support additional temporal coordinate reference systems and add
-additional enum values.
-
-default: http://www.opengis.net/def/uom/ISO-8601/0/Gregorian
-"""
-_COLLECTIONSCOLLECTIONIDGETRESPONSE200EXTENTTEMPORALTRS_HTTP_COLON__SOLIDUS__SOLIDUS_WWW_FULL_STOP_OPENGIS_FULL_STOP_NET_SOLIDUS_DEF_SOLIDUS_UOM_SOLIDUS_ISO_8601_SOLIDUS_0_SOLIDUS_GREGORIAN: Literal[
-    "http://www.opengis.net/def/uom/ISO-8601/0/Gregorian"
-] = "http://www.opengis.net/def/uom/ISO-8601/0/Gregorian"
-"""The values for the 'Coordinate reference system of the coordinates in the temporal extent' enum"""
-
-
-_Collectionscollectionidgetresponse200LinksItem = Any
-""" WARNING: we get an schema without any type """
 
 
 class _ComponentsSchemasCollection(TypedDict, total=False):
@@ -498,6 +389,12 @@ class _ComponentsSchemasCollection(TypedDict, total=False):
     """ Required property """
 
     extent: "_ComponentsSchemasExtent"
+    """
+    The extent of the features in the collection. In the Core only spatial and temporal
+    extents are specified. Extensions may add additional members to represent other
+    extents, for example, thermal or pressure ranges.
+    """
+
     itemType: str
     """
     indicator about the type of the items in the collection (the default value is 'feature').
@@ -522,7 +419,10 @@ class _ComponentsSchemasExtent(TypedDict, total=False):
     """
 
     spatial: "_ComponentsSchemasExtentSpatial"
+    """ The spatial extent of the features in the collection. """
+
     temporal: "_ComponentsSchemasExtentTemporal"
+    """ The temporal extent of the features in the collection. """
 
 
 class _ComponentsSchemasExtentSpatial(TypedDict, total=False):
@@ -539,9 +439,20 @@ class _ComponentsSchemasExtentSpatial(TypedDict, total=False):
     """
 
     crs: "_ComponentsSchemasExtentSpatialCrs"
+    """
+    Coordinate reference system of the coordinates in the spatial extent
+    (property `bbox`). The default reference system is WGS 84 longitude/latitude.
+    In the Core this is the only supported coordinate reference system.
+    Extensions may support additional coordinate reference systems and add
+    additional enum values.
+
+    default: http://www.opengis.net/def/crs/OGC/1.3/CRS84
+    """
 
 
-_ComponentsSchemasExtentSpatialBboxItem = list[Union[int, float]]
+_ComponentsSchemasExtentSpatialBboxItem = Union[
+    "_ComponentsSchemasExtentSpatialBboxItemOneof0", "_ComponentsSchemasExtentSpatialBboxItemOneof1"
+]
 """
 Each bounding box is provided as four or six numbers, depending on
 whether the coordinate reference system includes a vertical axis
@@ -569,6 +480,26 @@ the bottom and the top of the 3-dimensional bounding box.
 If a feature has multiple spatial geometry properties, it is the decision of the
 server whether only a single spatial geometry property is used to determine
 the extent or all relevant geometries.
+
+Aggregation type: oneOf
+"""
+
+
+_ComponentsSchemasExtentSpatialBboxItemOneof0 = Union[
+    str, Union[int, float], dict[str, Any], None, bool, None
+]
+"""
+minItems: 4
+maxItems: 4
+"""
+
+
+_ComponentsSchemasExtentSpatialBboxItemOneof1 = Union[
+    str, Union[int, float], dict[str, Any], None, bool, None
+]
+"""
+minItems: 6
+maxItems: 6
 """
 
 
@@ -603,6 +534,15 @@ class _ComponentsSchemasExtentTemporal(TypedDict, total=False):
     """
 
     trs: "_ComponentsSchemasExtentTemporalTrs"
+    """
+    Coordinate reference system of the coordinates in the temporal extent
+    (property `interval`). The default reference system is the Gregorian calendar.
+    In the Core this is the only supported temporal coordinate reference system.
+    Extensions may support additional temporal coordinate reference systems and add
+    additional enum values.
+
+    default: http://www.opengis.net/def/uom/ISO-8601/0/Gregorian
+    """
 
 
 _ComponentsSchemasExtentTemporalIntervalItem = list["_ComponentsSchemasExtentTemporalIntervalItemItem"]
