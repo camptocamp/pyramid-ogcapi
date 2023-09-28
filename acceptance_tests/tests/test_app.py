3b1a9037697490e5e775b4ff872eb80c1bfac25c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-09-28 17:43:51 +0200

    Add image test of output

--- acceptance_tests/tests/test_app.py
+++ acceptance_tests/tests/test_app.py
@@ -1,4 +1,7 @@
+import os
+
 import pytest
+from c2cwsgiutils.acceptance import image
 
 
 @pytest.mark.usefixtures("test_app")
@@ -32,3 +35,25 @@ def test_api(test_app, accept, f_param, expected):
     )
     assert response.status_code == 200
     assert response.headers["Content-Type"] == expected
+
+
+# Not working because we can't set the Host header to localhost:9123
+# def test_ogcapi():
+#    image.check_screenshot(
+#        "http://localhost:8080/ogcapi/",
+#        headers={"Accept": "text/html", "Host": "localhost:9123"},
+#        width=500,
+#        height=500,
+#        result_folder="results",
+#        expected_filename=os.path.join(os.path.dirname(__file__), "ogcapi.expected.png"),
+#    )
+
+
+def test_docs():
+    image.check_screenshot(
+        "http://localhost:8080/ogcapi/docs/",
+        width=900,
+        height=900,
+        result_folder="results",
+        expected_filename=os.path.join(os.path.dirname(__file__), "docs.expected.png"),
+    )
